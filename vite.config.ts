7deafdcea6780d3ba639b85a99485294c139a841
adammKass <adam.kascak@protonmail.com>
2025-09-28 14:49:05 +0200

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: `/VFXSTUDIO/`,
 
   test: {
     projects: [
